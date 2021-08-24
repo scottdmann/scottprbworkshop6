6a99d5e09b0b661d65c883fe907f61f717ad0ba5
Scott Mann <scott.mann@me.com>
2021-08-25 01:07:24 +1000

    basic node app with a test case, fixed bug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
     // Respond with "hello world" for requests that hit our root "/"
     app.get("/", function (req, res) {
-     return res.send("Helloo World");
+     return res.send("Hello World");
     });
 
     // listen to port 8000 by default
